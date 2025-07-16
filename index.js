47edf33c5814ce94eb6e1819e8fb1dd7cdea56bb
ForsDev101 <forsdeving@gmail.com>
2025-07-16 18:31:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-{
-  "name": "elforsa-discord-bot",
-  "version": "1.0.0",
-  "description": "ElForsa Discord botu",
-  "main": "index.js",
-  "scripts": {
-    "start": "node index.js"
-  },
-  "engines": {
-    "node": ">=18.0.0"
-  },
-  "dependencies": {
-    "discord.js": "^14.13.0",
-    "dotenv": "^16.3.1"
-  }
 // ElForsa Bot - Genişletilmiş Komutlar ve Sistemler
 const { Client, GatewayIntentBits, Partials, Collection, EmbedBuilder, PermissionsBitField } = require("discord.js");
 const noblox = require("noblox.js");
